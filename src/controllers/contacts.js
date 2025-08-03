bf20b87f7f9ec783df64abe64fb260f261643424
Igor <80968651+webdevbiv@users.noreply.github.com>
2025-08-03 22:22:37 +0300

    FIX | errorHandler.js

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -2,9 +2,15 @@ import mongoose from 'mongoose';
 import createError from 'http-errors';
 import { getAllContacts, getContactById } from '../services/contacts.js';
 import { HttpStatus, Messages } from '../constants/index.js';
+import { logger } from '../utils/logger.js';
 
 export const handleGetAllContacts = async (req, res) => {
   const data = await getAllContacts();
+
+  logger.info(
+    `[${req.method}] ${req.originalUrl} -> Fetched ${data.length} contacts`,
+  );
+
   res.status(HttpStatus.OK).json({
     status: HttpStatus.OK,
     message: Messages.CONTACTS_FETCHED,
@@ -16,17 +22,27 @@ export const handleGetContactById = async (req, res) => {
   const { contactId } = req.params;
 
   if (!mongoose.isValidObjectId(contactId)) {
+    logger.warn(
+      `[${req.method}] ${req.originalUrl} -> Invalid ID: ${contactId}`,
+    );
     throw createError(HttpStatus.BAD_REQUEST, Messages.INVALID_ID(contactId));
   }
 
   const contact = await getContactById(contactId);
 
   if (!contact) {
+    logger.warn(
+      `[${req.method}] ${req.originalUrl} -> Contact not found: ${contactId}`,
+    );
     throw createError(
       HttpStatus.NOT_FOUND,
       Messages.CONTACT_NOT_FOUND(contactId),
     );
   }
+
+  logger.info(
+    `[${req.method}] ${req.originalUrl} -> Contact found: ${contactId}`,
+  );
 
   res.status(HttpStatus.OK).json({
     status: HttpStatus.OK,
--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,11 +1,12 @@
 import createError from 'http-errors';
 import { logger } from '../utils/logger.js';
 
-export function errorHandler(err, req, res) {
+export function errorHandler(err, req, res, next) {
   const error =
     err.status && err.expose
       ? err
       : createError(err.status || 500, err.message, { cause: err });
+
   const { status = 500, message, stack } = error;
 
   logger.error(`[${req.method}] ${req.originalUrl} -> ${status}: ${message}`);
